78797ac09ecccb81686afe0335dc6ea54f3c62cf
Nicolas Filliol <nicolas.filliol@icloud.com>
2023-11-21 16:02:30 +0100

    Changed test to use pytest

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,52 +1,85 @@
-import unittest
 import numpy as np
+import pytest
 from vo.primitives.features import Features
 
 
-class TestFeatures(unittest.TestCase):
-    def setUp(self):
-        self.keypoints = np.array([[1, 2], [3, 4], [5, 6]])
-        self.descriptors = np.array([[7, 8], [9, 10], [11, 12]])
-        self.landmarks = np.array([[13, 14, 15], [16, 17, 18], [19, 20, 21]])
-        self.features = Features(self.keypoints, self.descriptors, self.landmarks)
+def test_features_initialization():
+    keypoints = np.array([[1, 2], [3, 4]])
+    descriptors = np.array([[5, 6], [7, 8]])
+    landmarks = np.array([[9, 10, 11], [12, 13, 14]])
 
-    def test_init(self):
-        self.assertTrue(np.array_equal(self.features.keypoints, self.keypoints))
-        self.assertTrue(np.array_equal(self.features.descriptors, self.descriptors))
-        self.assertTrue(np.array_equal(self.features.landmarks, self.landmarks))
-        self.assertTrue(np.array_equal(self.features.inliers, np.ones(3, dtype=bool)))
+    features = Features(keypoints, descriptors, landmarks)
 
-    def test_set_descriptors(self):
-        new_descriptors = np.array([[22, 23], [24, 25], [26, 27]])
-        with self.assertRaises(AssertionError):
-            self.features.descriptors = new_descriptors
+    assert np.array_equal(features.keypoints, keypoints)
+    assert np.array_equal(features.descriptors, descriptors)
+    assert np.array_equal(features.landmarks, landmarks)
 
-    def test_set_landmarks(self):
-        new_landmarks = np.array([[28, 29, 30], [31, 32, 33], [34, 35, 36]])
-        with self.assertRaises(AssertionError):
-            self.features.landmarks = new_landmarks
 
-    def test_set_inliers(self):
-        new_inliers = np.array([True, False, True])
-        self.features.inliers = new_inliers
-        self.assertTrue(np.array_equal(self.features.inliers, new_inliers))
+def test_features_default_initialization():
+    keypoints = np.array([[1, 2], [3, 4]])
 
-    def test_length(self):
-        self.assertEqual(self.features.length, 3)
+    features = Features(keypoints)
 
-    def test_apply_inliers(self):
-        new_inliers = np.array([True, False, True])
-        self.features.apply_inliers(new_inliers)
-        self.assertTrue(
-            np.array_equal(self.features.keypoints, self.keypoints[new_inliers])
-        )
-        self.assertTrue(
-            np.array_equal(self.features.descriptors, self.descriptors[new_inliers])
-        )
-        self.assertTrue(
-            np.array_equal(self.features.landmarks, self.landmarks[new_inliers])
-        )
+    assert np.array_equal(features.keypoints, keypoints)
+    assert features.descriptors is None
+    assert features.landmarks is None
+
+
+def test_features_properties():
+    keypoints = np.array([[1, 2], [3, 4]])
+    descriptors = np.array([[5, 6], [7, 8]])
+    landmarks = np.array([[9, 10, 11], [12, 13, 14]])
+
+    features = Features(keypoints, descriptors, landmarks)
+
+    assert features.length == 2
+    assert np.array_equal(features.keypoints, keypoints)
+    assert np.array_equal(features.descriptors, descriptors)
+    assert np.array_equal(features.landmarks, landmarks)
+    assert np.array_equal(
+        features.inliers, np.ones(shape=(features.length)).astype(bool)
+    )
+
+
+def test_descriptors_setter():
+    keypoints = np.array([[1, 2], [3, 4]])
+    descriptors = np.array([[5, 6], [7, 8], [9, 10]])  # wrong number of descriptors
+
+    features = Features(keypoints)
+
+    with pytest.raises(AssertionError):
+        features.descriptors = descriptors
+
+    descriptors = np.array([[9, 10], [11, 12]])
+
+    features.descriptors = descriptors
+
+    with pytest.raises(AssertionError):
+        features.descriptors = descriptors  # descriptors already set
+
+    assert np.array_equal(features.descriptors, descriptors)
+
+
+def test_landmarks_setter():
+    keypoints = np.array([[1, 2], [3, 4]])
+    landmarks = np.array(
+        [[5, 6, 7], [8, 9, 10], [11, 12, 13]]
+    )  # wrong number of landmarks
+
+    features = Features(keypoints)
+
+    with pytest.raises(AssertionError):
+        features.landmarks = landmarks
+
+    landmarks = np.array([[9, 10, 11], [12, 13, 14]])
+
+    features.landmarks = landmarks
+
+    with pytest.raises(AssertionError):
+        features.landmarks = landmarks  # landmarks already set
+
+    assert np.array_equal(features.landmarks, landmarks)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.main()
