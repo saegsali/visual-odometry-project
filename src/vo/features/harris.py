005d696bf9c76de7ad2d6849838a663c3b5e445c
Marek Landert <malandert@ethz.ch>
2023-12-07 17:36:46 +0100

    add harris corner detection

--- src/vo/features/harris.py
+++ src/vo/features/harris.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from scipy.spatial.distance import cdist
+from scipy import signal
 
 from vo.helpers import (
     to_cartesian_coordinates,
@@ -14,14 +15,135 @@ from vo.algorithms import RANSAC
 class HarrisCornerDetector:
     def __init__(
         self,
-        match_lambda: float = 4.0,
+        patch_size: int = 9,
+        kappa: float = 0.08,
+        num_keypoints: int = 200,
+        nonmaximum_supression_radius: int = 5,
+        descriptor_radius: int = 9,
+        match_lambda: float = 5.0,
     ):
         """Initialize feature matcher and set parameters."""
+        self._patch_size = patch_size
+        self._kappa = kappa
+        self._num_keypoints = num_keypoints
+        self._nonmaximum_supression_radius = nonmaximum_supression_radius
+        self._descriptor_radius = descriptor_radius
         self._match_lambda = match_lambda
 
-    def extractFeatures(self):
+    def to_gray(self, frame) -> Frame:
+        """Converts the image to a grayscale image."""
+        img = frame.image
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return Frame(gray_img)
+
+    def extractKeypoints(self, frame) -> Frame:
+        """Calculates the harris scores for an image given a patch size and a kappa value
+        Selects the best scores as keypoints and performs non-maximum supression of a (2r + 1)*(2r + 1) box around
+        the current maximum.
+
+        Args:
+            frame (Frame): object containing an image
+
+        Returns:
+            keypoints (Frame): object containing the input image and its keypoints
+        """
         pass
-        #TODO
+        # Get image from input frame
+        img = frame.image
+
+        # Init sobel filters in both directions
+        sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+
+        sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+        # Calculate the convolutions filters and image needed for the harris score
+        I_x = signal.convolve2d(sobel_x, img, mode="valid", boundary="symm")
+        I_y = signal.convolve2d(sobel_y, img, mode="valid", boundary="symm")
+
+        I_x2 = I_x**2
+        I_y2 = I_y**2
+        I_xy = I_x * I_y
+
+        patch = np.ones(shape=(self._patch_size, self._patch_size))
+        patch_radius = self._patch_size // 2
+
+        I_x2_sum = signal.convolve2d(patch, I_x2, mode="valid", boundary="symm")
+        I_y2_sum = signal.convolve2d(patch, I_y2, mode="valid", boundary="symm")
+        I_xy_sum = signal.convolve2d(patch, I_xy, mode="valid", boundary="symm")
+
+        # Calculate the harris scores of the image of the input frame
+        trace = I_x2_sum + I_y2_sum
+        determinant = I_x2_sum * I_y2_sum - I_xy_sum**2
+
+        harris_scores = determinant - self._kappa * (trace**2)
+        harris_scores[harris_scores < 0] = 0
+
+        harris_scores = np.pad(
+            harris_scores,
+            [
+                (patch_radius + 1, patch_radius + 1),
+                (patch_radius + 1, patch_radius + 1),
+            ],
+            mode="constant",
+            constant_values=0,
+        )
+
+        # Get shape of Harris score array (image + padding)
+        h, w = harris_scores.shape
+        r = self._nonmaximum_supression_radius
+
+        # Init array containing the keypoints
+        kp = np.zeros([self._num_keypoints, 2, 1])
+
+        # Get pixel cooridates of the (num_keypoints) keypoints with the highest harris scores
+        # and set pixels with radius (nonmaximum_supression_radius) around keypoint to 0 to not select it again as keypoint
+        for i in range(self._num_keypoints):
+            h_max, w_max = np.argmax(harris_scores) // w, np.argmax(harris_scores) % w
+            harris_scores[h_max - r : h_max + r + 1, w_max - r : w_max + r + 1] = 0
+
+            kp[i, :, :] = np.array([[h_max], [w_max]])
+
+        # Init Frame with keypoints
+        kp_feature = Features(kp)
+        keypoints = Frame(img, kp_feature)
+
+        return keypoints
+
+    def extractDescriptors(self, frame) -> Frame:
+        """
+        Returns a Frame with a N x (2*r+1)^2 x 1 matrix of image patch vectors describing the extracted keypoints of the Frame.
+        r is the descriptor_radius.
+
+        Args:
+            frame (Frame): object containing an image and its keypoints
+
+        Returns:
+            descriptors (Frame): object containing the input image, its keypoints and descriptors
+        """
+        # Get image and keypoints from input frame
+        img = frame.image
+        keypoints = frame.features.keypoints
+
+        r = self._descriptor_radius
+        N = self._num_keypoints
+
+        # Init descriptor matrix and padded image
+        desc = np.zeros([N, (2 * r + 1) ** 2, 1])
+        padded = np.pad(img, [(r, r), (r, r)], mode="constant", constant_values=0)
+
+        # For all keypoints, extract the descriptor patch around it
+        for kp in range(N):
+            patch = padded[
+                int(keypoints[kp, 0, 0]) - r + r : int(keypoints[kp, 0, 0]) + r + 1 + r,
+                int(keypoints[kp, 1, 0]) - r + r : int(keypoints[kp, 1, 0]) + r + 1 + r,
+            ]
+            desc[kp, :, :] = patch.reshape(-1, 1)
+
+        # Init Frame with keypoints and descriptors
+        desc_feature = Features(keypoints, desc)
+        descriptors = Frame(img, desc_feature)
+
+        return descriptors
 
     def matchDescriptor(self, frame1, frame2) -> Matches:
         """Matches the features of two frames based on their descriptors.
@@ -36,10 +158,15 @@ class HarrisCornerDetector:
         # descriptors should have shape (nr_of_keypoints, size_of_descriptor)
         descriptors1 = frame1.features.descriptors
         descriptors2 = frame2.features.descriptors
+        num_desc1 = descriptors1.shape[0]
+        num_desc2 = descriptors2.shape[0]
 
         # compute euclidean distance of every descriptor pair
-        dists = cdist(descriptors1, descriptors2, "euclidean")
-        # dists = np.linalg.norm(descriptors1, descriptors2)
+        dists = cdist(
+            descriptors1.reshape([num_desc1, -1]),
+            descriptors2.reshape([num_desc2, -1]),
+            "euclidean",
+        )
         min_non_zero_dist = np.min(dists)
         threshold = self._match_lambda * min_non_zero_dist
 
@@ -56,7 +183,9 @@ class HarrisCornerDetector:
 
         # get the row indices and stack them horizontally with the matches
         row_indices = np.indices(unique_matches.shape)
-        matches_pairs = np.hstack((row_indices.reshape(-1, 1), unique_matches.reshape(-1, 1)))
+        matches_pairs = np.hstack(
+            (row_indices.reshape(-1, 1), unique_matches.reshape(-1, 1))
+        )
 
         # create a boolean mask for rows where any number is smaller than 0
         mask = (matches_pairs < 0).any(axis=1)
@@ -75,27 +204,37 @@ if __name__ == "__main__":
     from vo.primitives.loader import Sequence
 
     # Load sequence
-    sequence = Sequence("malaga", camera=1, use_lowres=True)
+    sequence = Sequence("parking", camera=1, use_lowres=True)
     frame1 = sequence.get_frame(0)
-    frame2 = sequence.get_frame(1)
-
-    # Compute matches using SIFT (TODO: replace later with our FeatureMatcher)
-    sift = cv2.SIFT_create()
-    keypoints1, descriptors1 = sift.detectAndCompute(frame1.image, None)
-    keypoints2, descriptors2 = sift.detectAndCompute(frame2.image, None)
-
-    keypoints1 = np.array([kp.pt for kp in keypoints1]).reshape(-1, 2, 1)
-    keypoints2 = np.array([kp.pt for kp in keypoints2]).reshape(-1, 2, 1)
-    descriptors1 = np.array(descriptors1)
-    descriptors2 = np.array(descriptors2)
-
-    frame1.features = Features(keypoints1, descriptors1)
-    frame2.features = Features(keypoints2, descriptors2)
+    frame2 = sequence.get_frame(6)
 
     # Create an instance of the HarrisCornerDetector class
-    harris = HarrisCornerDetector(match_lambda=4.0) 
+    harris = HarrisCornerDetector(match_lambda=5, nonmaximum_supression_radius=5)
+
+    frame1 = harris.to_gray(frame1)
+    frame1 = harris.extractKeypoints(frame1)
+    frame1 = harris.extractDescriptors(frame1)
+
+    frame2 = harris.to_gray(frame2)
+    frame2 = harris.extractKeypoints(frame2)
+    frame2 = harris.extractDescriptors(frame2)
+
+    # # Compute matches using SIFT (TODO: replace later with our FeatureMatcher)
+    # sift = cv2.SIFT_create()
+    # keypoints1, descriptors1 = sift.detectAndCompute(frame1.image, None)
+    # keypoints2, descriptors2 = sift.detectAndCompute(frame2.image, None)
+
+    # keypoints1 = np.array([kp.pt for kp in keypoints1]).reshape(-1, 2, 1)
+    # keypoints2 = np.array([kp.pt for kp in keypoints2]).reshape(-1, 2, 1)
+    # descriptors1 = np.array(descriptors1)
+    # descriptors2 = np.array(descriptors2)
+
+    # frame1.features = Features(keypoints1, descriptors1)
+    # frame2.features = Features(keypoints2, descriptors2)
+
     # Call the matchDescriptor method
-    matches = harris.matchDescriptor(frame1, frame2) 
+    matches = harris.matchDescriptor(frame1, frame2)
+    print('number of matches:', matches.frame1.features.keypoints.shape[0])
 
     # Visualize
     img1 = matches.frame1.image
@@ -108,19 +247,25 @@ if __name__ == "__main__":
         H, W = img.shape[:2]
 
         for i, keypoint in enumerate(keypoints):
-            print(keypoint)
+            # print(keypoint)
             x1 = keypoint[0].astype(int)
             y1 = keypoint[1].astype(int)
-            print(x1.reshape(-1).shape)
-            img = cv2.circle(img, (x1[0],y1[0]), radius=2, color=((10*i)%255,i%255,255), thickness=2)
+            # print(x1.reshape(-1).shape)
+            img = cv2.circle(
+                img,
+                (x1[0], y1[0]),
+                radius=2,
+                color=((10 * i) % 255, i % 255, 255),
+                thickness=2,
+            )
+            # print(i)
         return img
 
-    img1 = draw_keypoints(img1, keypoint1)
-    img2 = draw_keypoints(img2, keypoint2)
+    img1 = draw_keypoints(img1, np.flip(keypoint1, axis=1))
+    img2 = draw_keypoints(img2, np.flip(keypoint2, axis=1))
 
     img_both = cv2.hconcat([img1, img2])
 
     cv2.imshow("Keypoints", img_both)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
--- tests/test_harris.py
+++ tests/test_harris.py
@@ -4,10 +4,11 @@ from vo.algorithms import RANSAC
 from vo.features import HarrisCornerDetector
 import pytest
 from scipy.spatial.distance import cdist
+from vo.primitives.loader import Sequence
 
 
 @pytest.fixture
-def sample_frames():
+def sample_frames1():
     # Create dummy data for testing
     descriptors1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     descriptors2 = np.array([[2, 3, 4], [5, 6, 7], [8, 9, 10]])
@@ -28,8 +29,8 @@ def sample_frames():
     return frame1, frame2
 
 
-def test_matchDescriptor(sample_frames):
-    frame1, frame2 = sample_frames
+def test_matchDescriptor(sample_frames1):
+    frame1, frame2 = sample_frames1
 
     # Create an instance of the HarrisCornerDetector class
     matcher = HarrisCornerDetector(match_lambda=4.0)
@@ -47,9 +48,9 @@ def test_matchDescriptor(sample_frames):
     )
 
 
-def test_matchDescriptor_different_lambda(sample_frames):
+def test_matchDescriptor_different_lambda(sample_frames1):
     # Test when using a large match_lambda, no matches should be found
-    frame1, frame2 = sample_frames
+    frame1, frame2 = sample_frames1
 
     large_lambda = 100.0
     matcher_l = HarrisCornerDetector(match_lambda=large_lambda)
@@ -119,4 +120,72 @@ def test_matchDescriptor_many_keypoints():
     )
 
 
+@pytest.fixture
+def sample_frames2():
+    # Create dummy data for testing
+    # img1 = np.zeros((100, 100, 3), dtype=np.uint8)
+    # img2 = np.zeros((100, 100, 3), dtype=np.uint8)
+
+    # img1 = np.random.rand(100, 100, 1)
+    # img2 = img1
+
+    # frame1 = Frame(image=img1)
+    # frame2 = Frame(image=img2)
+
+    # Load sequence
+    sequence = Sequence("parking", camera=1, use_lowres=True)
+    frame1 = sequence.get_frame(0)
+    frame2 = sequence.get_frame(1)
+
+    return frame1, frame2
+
+
+def test_HarrisCornerDetector(sample_frames2):
+    frame1, frame2 = sample_frames2
+
+    # Create an instance of the HarrisCornerDetector class
+    harris = HarrisCornerDetector(num_keypoints=200)
+
+    # Convert frames to grayscale
+    frame1_gray = harris.to_gray(frame1)
+    frame2_gray = harris.to_gray(frame2)
+    # frame1_gray = frame1
+    # frame2_gray = frame2
+
+    # Extract keypoints from the frames
+    frame1_keypoints = harris.extractKeypoints(frame1_gray)
+    frame2_keypoints = harris.extractKeypoints(frame2_gray)
+
+    # Extract descriptors from the frames
+    frame1_descriptors = harris.extractDescriptors(frame1_keypoints)
+    frame2_descriptors = harris.extractDescriptors(frame2_keypoints)
+
+    # Call the matchDescriptor method
+    matches = harris.matchDescriptor(frame1_descriptors, frame2_descriptors)
+
+    # Ensure that the result is of type Matches
+    assert isinstance(matches, Matches)
+
+    # Add assertions based on your expectations for the test case
+    # For example, you can check the number of keypoints, descriptors, and matches
+    assert (
+        frame1_keypoints.features.keypoints.shape[0] == 200
+    )  # Assuming 200 keypoints are extracted
+    assert (
+        frame1_descriptors.features.descriptors.shape[0] == 200
+    )  # Assuming 200 descriptors are extracted
+    assert (
+        matches.frame1.features.keypoints[0][0][0]
+        == matches.frame2.features.keypoints[0][0][0]
+    )  # First matches keypoints have the same x-coordinate
+    assert (
+        matches.frame1.features.descriptors.shape
+        == matches.frame2.features.descriptors.shape
+    )  # Same amount of matches keypoints in both frames
+    assert (
+        len(matches.frame1.features.keypoints) > 0
+    )  # Assuming at least one match is found
+
+
+# Run the test using pytest
 # Command: pytest test_harris.py
